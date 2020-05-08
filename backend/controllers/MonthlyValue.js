4f25131c81daa13371ed90e0dcee6cc72098a097
Chawintee <Chawinteetea@gmail.com>
2020-05-08 23:42:03 +0700

    can connected MonthlyValueroute

--- backend/controllers/MonthlyValue.js
+++ backend/controllers/MonthlyValue.js
@@ -1,10 +1,27 @@
 const db = require('../models/MonthlyValue');
 
+const createMonthlyValue = (req, res) => {
+
+    const Year = req.body.Year;
+    const Month = req.body.Month;
+    const WaterMeter = req.body.WaterMeter;
+    const WaterPricePerUnit = req.body.WaterPricePerUnit;
+    const WaterPrice = req.body.WaterPrice;
+    const ElectricityMeter = req.body.ElectricityMeter;
+    const ElectricityPricePerUnit = req.body.ElectricityPricePerUnit;
+    const ElectricityPrice = req.body.ElectricityPrice;
+    const RentPrice = req.body.RentPrice;
+    const TotalRentPrice = req.body.TotalRentPrice;
+    const PaidStatus = req.body.PaidStatus;
+    const PaidDate = req.body.PaidDate;
+
+    res.send(Year)
+
+}
 
 
 
 
 
 
-
-module.exports = {}
+module.exports = { createMonthlyValue, }
--- backend/routes/MonthlyValue.js
+++ backend/routes/MonthlyValue.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const MonthlyValueController = require('../controllers/MonthlyValue');
 
-router.post('/createMonthlyValue',)
+router.post('/createMonthlyValue',MonthlyValueController.createMonthlyValue)
 router.patch('/editSomeValueById',)
 
 
