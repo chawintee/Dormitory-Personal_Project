e02bf98884915e692ef554e24093e4c15a2c83be
Chawintee <Chawinteetea@gmail.com>
2020-05-08 23:35:24 +0700

    create MonthlyValueController

---
+++ backend/controllers/MonthlyValue.js
@@ -0,0 +1,10 @@
+const db = require('../models/MonthlyValue');
+
+
+
+
+
+
+
+
+module.exports = {}
--- backend/routes/MonthlyValue.js
+++ backend/routes/MonthlyValue.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const MonthlyValueController = require('../controllers/MonthlyValue');
 
-
+router.post('/createMonthlyValue',)
+router.patch('/editSomeValueById',)
 
 
 
