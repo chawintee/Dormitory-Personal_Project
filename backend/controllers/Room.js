8827a30c840a9b7fdd4f1f79d7052cf9f55202dc
Chawintee <Chawinteetea@gmail.com>
2020-05-08 22:35:22 +0700

    can create room

--- backend/controllers/Room.js
+++ backend/controllers/Room.js
@@ -1,25 +1,31 @@
 const db = require('../models');
 
-const createRoom =async (req,res) => {
+const createRoom = async (req, res) => {
     const RoomNumber = req.body.RoomNumber;
     const Floor = req.body.Floor;
     const LessonId = req.body.LessonId;
+    console.log(RoomNumber);
+    const room = await db.Room.findOne({ where: { RoomNumber: RoomNumber ,LessonId } });
+    // res.send(room)
 
-    await db.Room.create({
-        RoomNumber,
-        Floor,
-        LessonId,
-    })
-
-    res.status(201).send("Room created")
+    if (room ) {
+        res.status(400).send({ message: "Room already token" })
+    } else {
+        await db.Room.create({
+            RoomNumber,
+            Floor,
+            LessonId,
+        })
+        res.status(201).send({message:"Room created"})
+    }
 }
 
-const getRoomByLessonId = async (req,res) => {
+const getRoomByLessonId = async (req, res) => {
     const LessonId = req.params.LessonId;
-    const result = await db.Room.findAll({where: {LessonId:LessonId}});
-    res.status(200).send();
+    const result = await db.Room.findAll({ where: { LessonId: LessonId } });
+    res.status(200).send(result);
 }
 
 
 
-module.exports = {createRoom,getRoomByLessonId}
+module.exports = { createRoom, getRoomByLessonId }
