a4383fb3b7df82a98b83e5acb9532a47e9a4ab14
Chawintee <Chawinteetea@gmail.com>
2020-05-10 22:52:19 +0700

    crate error path

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Landing from './Landing'
+import Error from './Error'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={Landing} />
+        <Route exact path='/error' component={Error} />
+        <Redirect to='/error' />
       </Switch>
     </div>
   );
