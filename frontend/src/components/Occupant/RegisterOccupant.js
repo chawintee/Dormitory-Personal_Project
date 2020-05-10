e149b9d67652d5e7d19becf3b122073c50b289a8
Chawintee <Chawinteetea@gmail.com>
2020-05-10 22:46:40 +0700

    create file frontend

---
+++ frontend/src/components/Occupant/RegisterOccupant.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterOccupant() {
+    return (
+        <div>
+            <h1>RegisterOccupant</h1>
+        </div>
+    )
+}
+
+export default RegisterOccupant
