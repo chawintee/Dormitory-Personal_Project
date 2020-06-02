71ed55e12e8a891e45735c3bb1efe4413ef73f3a
Chawintee <Chawinteetea@gmail.com>
2020-06-02 12:00:22 +0700

    add ManageOcuupantPage

---
+++ frontend/src/components/Lesson/ManageOccupant.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ManageOccupant() {
+    return (
+        <div>
+            Hello manage ManageOccupant
+        </div>
+    )
+}
+
+export default ManageOccupant
